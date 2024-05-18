c7505ba52cda200cdd790174f57885c4c47481e7
Diego Carmona Bernal <cbdiegox@gmail.com>
2024-05-18 11:34:39 -0600

    update

--- main.py
+++ main.py
@@ -7,34 +7,47 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-reserved_words = {'for', 'do', 'while', 'if',
-                  'int', 'else', 'printf', 'end', 'read'}
+identifiers = {'a', 'b', 'c', 'la', 'suma', 'es'}
+reserved_words = {'for', 'do', 'while', 'if', 'int',
+                  'else', 'print', 'end', 'read', 'programa'}
 symbols = {';', '"', '+', '=', ',', '(', ')', '{', '}'}
 
 
 def analyze_code(code):
     lines = code.split('\n')
     tokens = []
+    errors = []
 
     for i, line in enumerate(lines, start=1):
-        # Utilizar expresiones regulares para encontrar palabras y símbolos
         words = re.findall(r'\b\w+\b|[\(\){};"+=,]', line)
         for word in words:
+            if word in reserved_words:
+                reserved = 'x'
+            else:
+                reserved = ''
+            if word not in reserved_words and word not in symbols and not word.isdigit() and word not in identifiers:
+                errors.append({'token': word, 'line': i})
             token = {
                 'token': word,
-                'linea': i,
-                'reservada': 'x' if word in reserved_words else '',
-                'simbolo': 'x' if word in symbols else '',
-                'parentesis_izquierdo': 'x' if word == '(' else '',
-                'parentesis_derecho': 'x' if word == ')' else '',
-                'llave_izquierda': 'x' if word == '{' else '',
-                'llave_derecha': 'x' if word == '}' else '',
-                'numero': 'x' if word.isdigit() else '',
-                'identificador': 'x' if word not in reserved_words and word not in symbols and not word.isdigit() else ''
+                'line': i,
+                'reserved': reserved,
+                'symbol': 'x' if word in symbols else '',
+                'comma': 'x' if word == ',' else '',
+                'semicolon': 'x' if word == ';' else '',
+                'left_paren': 'x' if word == '(' else '',
+                'right_paren': 'x' if word == ')' else '',
+                'left_brace': 'x' if word == '{' else '',
+                'right_brace': 'x' if word == '}' else '',
+                'number': 'x' if word.isdigit() else '',
+                'identifier': 'x' if word in identifiers else '',
             }
             tokens.append(token)
 
-    return tokens
+    total_counts = {key: sum(
+        1 for token in tokens if token[key] == 'x') for key in tokens[0].keys() if key != 'token'}
+    tokens.append(total_counts)
+
+    return tokens, errors
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -49,20 +62,20 @@ def index():
             file.save(file_path)
             with open(file_path, 'r') as f:
                 code = f.read()
-            tokens = analyze_code(code)
-            return render_template('index.html', tokens=tokens, code=code)
+            tokens, errors = analyze_code(code)
+            return render_template('index.html', tokens=tokens, errors=errors, code=code)
         else:
             code = request.form['code']
-            tokens = analyze_code(code)
-            return render_template('index.html', tokens=tokens, code=code)
-    return render_template('index.html', tokens=[], code='')
+            tokens, errors = analyze_code(code)
+            return render_template('index.html', tokens=tokens, errors=errors, code=code)
+    return render_template('index.html', tokens=[], errors=[], code='')
 
 
 @app.route("/analyze", methods=['POST'])
 def analyze():
     code = request.json['code']
-    tokens = analyze_code(code)
-    return jsonify({'tokens': tokens})
+    tokens, errors = analyze_code(code)
+    return jsonify({'tokens': tokens, 'errors': errors})
 
 
 if __name__ == '__main__':
